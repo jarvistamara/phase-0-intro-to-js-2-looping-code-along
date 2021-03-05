a1eefc59d282c58fe07b86dd94292441ea2de354
Tamara Jarvis <tamara.jarvis17@icloud.com>
2021-03-05 11:09:11 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Code your solutions in this file
+const names = ['Lisa', 'Kaitlin', 'Jan'];
+
+function writeCards(names, event) {
+  let birthday =[];
+  event = "surprise";
+  for (let i= 0; i < names.length; i++) {
+  birthday[i] = 'Thank you, '+ names[i] +', for the wonderful ' +event+' gift!';
+  }
+  return birthday;
+}
+/*
+let number = 10;
+function countdown (number) {
+    while (number >= 10) {
+        console.log(number--);
+      }
+}*/
+
+//declare and instantiate your variable
+let number = 10;
+
+
+
+function countdown(number) {
+    while (number > 0) {
+    console.log(number);
+    number--;
+    }
+}
+
+alert("Done");
